fdedabf6ff6ec657d26d686d86e7aec877bf0744
AdrianoMouraSilva <adr.moura21@hotmail.com>
2025-03-15 16:38:46 -0300

    Case_2

--- condicionais.js
+++ condicionais.js
@@ -1,11 +1,31 @@
 let nota = 10;
 
-if(nota === 10){
-    return console.log ('Excelente')
-} else if(nota >= 7){
-    return console.log ('Aprovado')
-} else if(nota >= 4){
-    return console.log ('Recuperação')
-} else {
-    return console.log ('Reprovado')
+// if (nota === 10) {
+//     return console.log('Excelente')
+// } else if (nota >= 7) {
+//     return console.log('Aprovado')
+// } else if (nota >= 4) {
+//     return console.log('Recuperação')
+// } else {
+//     return console.log('Reprovado')
+// }
+
+
+switch (nota) {
+    case nota === 10:
+        return console.log('Excelente!')
+    // break;
+
+    case nota >= 7:
+        console.log('Aprova!')
+        break;
+
+    case nota >= 4:
+        return console.log('Aprova!')
+    // break;
+
+    default:
+        console.log('Reprovado!')
+
+        break;
 }
