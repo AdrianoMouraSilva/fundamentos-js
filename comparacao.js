cba28017af24d731491aa518db09a104ba9b2f16
AdrianoMouraSilva <adr.moura21@hotmail.com>
2025-03-15 10:24:02 -0300

    Comparação

---
+++ comparacao.js
@@ -0,0 +1,54 @@
+//---------------IGUALDADE ESTRITO---------------INICIO
+let igual;
+let string = '5'
+let number = 5
+
+igual = string == number;
+
+console.log('Comparação dois igual : '+ igual);
+
+igual = string === number;
+
+console.log("Comparação três igual : " + igual);
+let nome = 'Adriano';
+
+console.log(nome)
+//---------------IGUALDADE ESTRITO---------------FIM
+
+//---------------DIFERENTE---------------INICIO
+let diferente
+
+diferente = (string != number)
+
+console.log(diferente, 'diferente')
+
+diferente = (string !== number)
+
+console.log(diferente, 'diferente estrita')
+//---------------DIFERENTE---------------FIM
+
+//---------------MAIOR/MENOR/MENOR IGUAL/MAIOR IGUAL---------------INICIO
+
+let maior = (10 > 5)
+let menor = (7 < 3)
+let maiorIgual = (5 <= 5)
+let menorIgual = (3 >= 4)
+
+console.log('Maior :',maior);
+console.log('Menor :',menor);
+console.log('Maior Igual :',maiorIgual);
+console.log('Menor Igual :',menorIgual);
+
+//---------------MAIOR/MENOR/MENOR IGUAL/MAIOR IGUAL---------------FIM
+
+
+
+
+
+
+
+
+
+
+
+//---------------COMPARAÇÃO---------------FIM
