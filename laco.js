30af90604496487f73e05be607202d0a00d9e549
AdrianoMouraSilva <adr.moura21@hotmail.com>
2025-03-22 15:20:27 -0300

    Lista de exercícios (For)

---
+++ laco.js
@@ -0,0 +1,24 @@
+function contagem(limite) {
+    let somaPares = 0;
+    let somaImpares = 0;
+
+
+    for (let i = 0; i <= limite; i++) {
+        let resto = i % 2;
+
+        if (resto === 0) {
+            somaPares += i;
+            console.log("Número ", i, " é par : ");
+        }else{
+            somaImpares += i;
+        }
+        // console.log("Número ", i, " é par : "); 
+    }
+    return `A Soma de pares é ${somaPares}, e as dos ímpares ${somaImpares}` 
+
+//     console.log("Soma dos numero par :",soma);
+//     console.log("Soma dos numero impar :",soma);
+ }
+
+ console.log(contagem(10));
+
